cbbc276a9cd7f97339ebbf1453b6fe0737879651
Joe Schoonover <joe@fluidnumerics.com>
2020-10-08 16:45:30 -0600

    Revert "Add tensorflow to demo"
    This reverts commit 8e046db6c09e19117226cee97fb97b3f4938b44a.

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import sys
 import scipy
 import numpy
 import matplotlib
-import tensorflow
 
 print('scipy version : '+scipy.__version__)
 print('scipy module : '+scipy.__file__)
@@ -11,5 +10,3 @@ print('numpy version : '+numpy.__version__)
 print('numpy module : '+numpy.__file__)
 print('matplotlib version : '+matplotlib.__version__)
 print('matplotlib module : '+matplotlib.__file__)
-print('tensorflow version : '+tensorflow.__version__)
-print('tensorflow module : '+tensorflow.__file__)
